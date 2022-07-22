df4ad21e11d4e13918d843dd04559828af60b7b5
Mauro Goyeneche <maurogoyeneche@gmail.com>
2022-07-22 00:33:43 -0300

    Translate AboutAiki to spanish

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 // import Post from "../components/Post/Post";
-import PostList from "../components/PostList/PostList";
+// import PostList from "../components/PostList/PostList";
 import AboutAiki from "../screens/about/AboutAiki";
 import Home from "../screens/home/Home";
-import DojoDetails from "../screens/dojo/DojoDetails";
+
 import ContactView from "../screens/contact/ContactView";
 // import DojoDetails from "../screens/dojo/DojoDetails";
 import DojoList from "../components/DojoList/DojoList";
@@ -14,10 +14,12 @@ const AppRouter = () => {
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />}>
-          <Route path="news" element={<PostList />} />
+          {/* <Route path="news" element={<PostList />} /> */}
           <Route path="about" element={<AboutAiki />} />
+          <Route index element={<ContactView />} />
           <Route path="contact" element={<ContactView />} />
           <Route path="dojo" element={<DojoList />} />
+          <Route path="*" element={<ContactView />} />
         </Route>
       </Routes>
     </Router>
