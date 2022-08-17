aa1d93fcfbf048473a0fa3b6abfc2c2517c0014b
Mauro Goyeneche <maurogoyeneche@gmail.com>
2022-08-16 22:23:05 -0300

    Fix Hero broke in an PR

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,21 +1,17 @@
 import React from "react";
+import styles from "./Hero.module.css";
 
-import style from "./Hero.module.css";
-
-
-
-function Hero({ news }) {
+function Hero() {
   return (
     <>
-      <div className={ styles.hero }>
+      <div className={styles.hero}>
         <div>
-          <h1>
-            AIKIDO TRADICIONAL
-          </h1>
+          <h1>AIKIDO TRADICIONAL</h1>
           <h4>Alumnos directos de Hitohira Saito soke.</h4>
           <button>CONTACTANOS</button>
         </div>
       </div>
+    </>
   );
 }
 
