cb6371a1c356a43475bb39cb5f4426f64987482f
Mauro Goyeneche <maurogoyeneche@gmail.com>
2023-07-09 00:32:53 -0300

    add vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import React from "react";
 import AppRouter from "./routers/AppRouter";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
       <AppRouter />
+      <Analytics />
     </>
   );
 }
